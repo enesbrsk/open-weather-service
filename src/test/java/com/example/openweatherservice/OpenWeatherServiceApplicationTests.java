f5871cd8d4543ca4141d5e2f291a13911fe90fa6
enesbrsk <enesbrsk@hotmail.com>
2023-04-28 17:44:59 +0300

    project init

---
+++ src/test/java/com/example/openweatherservice/OpenWeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openweatherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWeatherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
